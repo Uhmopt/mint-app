0e2c0e2a4e2a0c63213d41b66c43e39eb172340c
dragonMao26 <enginechair1959@gmail.com>
2021-11-26 06:39:41 +0200

    init home

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useMemo } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// import Home from "./Home";
+
+import * as anchor from "@project-serum/anchor";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  getPhantomWallet,
+  getSolflareWallet,
+  getSolletWallet,
+} from "@solana/wallet-adapter-wallets";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+
+const treasury = new anchor.web3.PublicKey(
+  process.env.REACT_APP_TREASURY_ADDRESS!
+);
+
+const config = new anchor.web3.PublicKey(
+  process.env.REACT_APP_CANDY_MACHINE_CONFIG!
+);
+
+const candyMachineId = new anchor.web3.PublicKey(
+  process.env.REACT_APP_CANDY_MACHINE_ID!
+);
+
+const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+
+const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const connection = new anchor.web3.Connection(rpcHost);
+
+const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
+
+const txTimeout = 30000; // milliseconds (confirm this works for your project)
+
+const App = () => {
+  const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+  const wallets = useMemo(
+    () => [getPhantomWallet(), getSolflareWallet(), getSolletWallet()],
+    []
   );
-}
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletDialogProvider>
+          <h1>hello world</h1>
+          {/* <Home
+            candyMachineId={candyMachineId}
+            config={config}
+            connection={connection}
+            startDate={startDateSeed}
+            treasury={treasury}
+            txTimeout={txTimeout}
+          /> */}
+        </WalletDialogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
 
 export default App;
